a2fb0d78dc3bbd3f8acee73eb4d6663fd1610125
Matheus <Mass.josse72@gmail.com>
2024-08-05 16:24:38 -0300

    lista tarefas

---
+++ js/scripts.js
@@ -0,0 +1,148 @@
+// seleção de elemento
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const chk = document.getElementById('chk')
+
+let oldInputValue;
+
+// funcao para dark
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+// Função para salvar a lista no localStorage
+const saveListToLocalStorage = () => {
+    const todos = [];
+    document.querySelectorAll(".todo h3").forEach((todoTitle) => {
+        todos.push({ text: todoTitle.innerText, done: todoTitle.parentElement.classList.contains("done") });
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Função para carregar a lista do localStorage
+const loadListFromLocalStorage = () => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos) {
+        todos.forEach((todo) => {
+            saveTodo(todo.text, todo.done);
+        });
+    }
+}
+
+// Função para salvar uma nova tarefa
+const saveTodo = (text, done = false) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+    if (done) {
+        todo.classList.add("done");
+    }
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    todoInput.focus();
+
+    saveListToLocalStorage();
+}
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+}
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTitle = todo.querySelector("h3");
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    });
+
+    saveListToLocalStorage();
+}
+
+// eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value;
+
+    if (inputValue) {
+        saveTodo(inputValue);
+    }
+});
+
+document.addEventListener("click", (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest("div");
+    let todoTitle;
+
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
+    }
+
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done");
+        saveListToLocalStorage();
+    }
+    if (targetEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+        saveListToLocalStorage();
+    }
+    if (targetEl.classList.contains("edit-todo")) {
+        toggleForms();
+
+        editInput.value = todoTitle;
+        oldInputValue = todoTitle;
+    }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const editInputValue = editInput.value;
+
+    if (editInputValue) {
+        updateTodo(editInputValue);
+    }
+    toggleForms();
+});
+
+// Carregar lista do localStorage quando a página for carregada
+document.addEventListener("DOMContentLoaded", () => {
+    loadListFromLocalStorage();
+});
+
+
